bd32286921ae40939a8c41c7fae0d16b7473894a
Shresth-Ojha <shresthojha.iitdelhi@gmail.com>
2024-03-14 05:40:32 +0530

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { AuthWrapper } from './store/storeWrapper.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-        <BrowserRouter>
-            <AuthWrapper>
-                <App />
-            </AuthWrapper>
-        </BrowserRouter>
+    <BrowserRouter>
+        <AuthWrapper>
+            <App />
+            <Analytics />
+        </AuthWrapper>
+    </BrowserRouter>
     // </React.StrictMode>
 );
